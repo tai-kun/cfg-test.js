1434b7c43d1cddaeefeae9894b50c8dbec55253d
tai-kun <taikun.dev@gmail.com>
2024-02-18 08:29:51 +0900

    Add define.mjs

---
+++ src/define.mjs
@@ -0,0 +1,33 @@
+export const testEnv = Object.freeze({
+  NODE_ENV: "test",
+  CFG_TEST: "true",
+});
+
+export const buildEnv = Object.freeze({
+  CFG_TEST: "false",
+  CFG_TEST_URL: undefined,
+  CFG_TEST_FILE: undefined,
+  CFG_TEST_WATCH: "false",
+});
+
+export const testDefine = Object.freeze({
+  "process.env.NODE_ENV": "\"test\"",
+  "process.env.CFG_TEST": "\"true\"",
+});
+
+export const buildDefine = Object.freeze({
+  "cfgTest": "undefined",
+  "cfgTest.url": "undefined",
+  "cfgTest?.url": "undefined",
+  "cfgTest!.url": "undefined",
+  "cfgTest.file": "undefined",
+  "cfgTest?.file": "undefined",
+  "cfgTest!.file": "undefined",
+  "cfgTest.watch": "false",
+  "cfgTest?.watch": "false",
+  "cfgTest!.watch": "false",
+  "process.env.CFG_TEST": "\"false\"",
+  "process.env.CFG_TEST_URL": "undefined",
+  "process.env.CFG_TEST_FILE": "undefined",
+  "process.env.CFG_TEST_WATCH": "\"false\"",
+});
